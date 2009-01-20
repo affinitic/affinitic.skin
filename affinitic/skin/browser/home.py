db451c8bccb2113baa6c5c857bb99c3652a6bec9
Gillian Sampont <gillian@affinitic.be>
2009-01-20 18:40:12 +0000

    add home

---
+++ affinitic/skin/browser/home.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+<+ MODULE_NAME +>
+
+Licensed under the <+ LICENSE +> license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id: event.py 67630 2006-04-27 00:54:03Z jfroche $
+"""
+
+from Products.Five.browser import BrowserView
+
+
+class HomeView(BrowserView):
+    """
+    """
+
+    pass
--- affinitic/skin/setuphandlers.py
+++ affinitic/skin/setuphandlers.py
@@ -1,3 +1,9 @@
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignmentMapping
+
+
 def setupVarious(context):
 
     # Ordinarily, GenericSetup handlers check for the existence of XML files.
@@ -9,3 +15,23 @@ def setupVarious(context):
         return
 
     # Add additional setup code here
+
+
+def getManager(folder, column):
+    if column == 'left':
+        manager = getUtility(IPortletManager, name=u'plone.leftcolumn', context=folder)
+    else:
+        manager = getUtility(IPortletManager, name=u'plone.rightcolumn', context=folder)
+    return manager
+
+
+def clearColumnPortlets(folder, column):
+    manager = getManager(folder, column)
+    assignments = getMultiAdapter((folder, manager, ), IPortletAssignmentMapping)
+    for portlet in assignments:
+        del assignments[portlet]
+
+
+def clearPortlets(folder):
+    clearColumnPortlets(folder, 'left')
+    clearColumnPortlets(folder, 'right')

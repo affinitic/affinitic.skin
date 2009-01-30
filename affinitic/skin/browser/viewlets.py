030c64400b5fc2adc80d00acc76f8d588d3dd4d5
Gillian Sampont <gillian@affinitic.be>
2009-01-30 16:02:59 +0000

    fix menu flash

--- affinitic/skin/browser/viewlets.py
+++ affinitic/skin/browser/viewlets.py
@@ -9,6 +9,10 @@ $Id: viewlets.py 4075 2009-01-19 14:53:19Z schminitz $
 """
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignmentMapping
 
 
 class AffiniticHeaderViewlet(GlobalSectionsViewlet):
@@ -18,6 +22,13 @@ class AffiniticHeaderViewlet(GlobalSectionsViewlet):
 class AffiniticContentViewlet(GlobalSectionsViewlet):
     render = ViewPageTemplateFile('templates/contentAffinitic.pt')
 
+    def bouge(self):
+        context = self.context
+        site = context.getSite()
+        column = getUtility(IPortletManager, name=u'plone.leftcolumn', context=site)
+        manager = getMultiAdapter((site, column, ), IPortletAssignmentMapping)
+        for portlet in manager:
+            del manager[portlet]
 
 class AffiniticFooterViewlet(GlobalSectionsViewlet):
     render = ViewPageTemplateFile('templates/footerAffinitic.pt')

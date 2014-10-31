f41e62969294df93a69e45edbae2ee800d573afe
Alain Meurant <alain.meurant@affinitic.be>
2014-10-31 12:26:42 +0000

    Travail background sepi et flou

--- affinitic/skin/browser/js/affinitic.js
+++ affinitic/skin/browser/js/affinitic.js
@@ -5,10 +5,10 @@ jQuery(function($){
 		transition : 1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
 		random : 1,
 		transition_speed : 2000,		// Speed of transition
-															   
-		// Components							
+
+		// Components
 		slide_links : 'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		slides:[ 
+		slides:[
 		        {image : 'bg_body_01.jpg'},
 		        {image : 'bg_body_02.jpg'},
 		        {image : 'bg_body_03.jpg'},
@@ -16,7 +16,8 @@ jQuery(function($){
 		        {image : 'bg_body_05.jpg'},
 		        {image : 'bg_body_06.jpg'},
 		        {image : 'bg_body_07.jpg'},
-		        {image : 'bg_body_08.jpg'}
+		        {image : 'bg_body_08.jpg'},
+		        {image : 'bg_body_09.jpg'}
 		        ]
 		});
 	});
